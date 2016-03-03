0644b34e13626e7787425c981f42030899fd27bd
qband <q7band@gmail.com>
2016-03-04 07:59:25 +0800

    update build script

--- build-binary.sh
+++ build-binary.sh
@@ -1,13 +1,18 @@
-git submodule update --init --recursive
+# initialize submodule or update submodule
+git submodule update --init --recursive 2>/dev/null
 git submodule foreach git pull origin master
 
 # rsync -rPq src/src.sourcegraph.com/sourcegraph/vendor/ vendor/src
 # rsync -rPq src/ $GOPATH/src/
 
+# get build tools for golang
 go get -u github.com/tools/godep
-pushd src/src.sourcegraph.com/sourcegraph
-godep restore -v
-popd
-
 go get -u github.com/constabulary/gb/...
+
+# download dependencies
+cd src/src.sourcegraph.com/sourcegraph
+GOPATH=$(pwd)/../../../vendor godep restore -v
+cd ../../../
+
+# build artifact
 gb build src.sourcegraph.com/sourcegraph/cmd/src
